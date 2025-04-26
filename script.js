c714088369f8a428a903cb4deeba458e2c05c5e9
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2025-04-26 14:53:05 +0800

    wip

---
+++ script.js
@@ -0,0 +1,22 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read){
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.read = read,
+    this.id = self.crypto.randomUUID();
+}
+
+
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
+
+addBookToLibrary('Hobbit','Juan',300,'read');
+console.log(myLibrary[0]);
+
+function displayBook(){
+
+}
