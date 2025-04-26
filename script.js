bcbbf67fbb8f7db7d591420921b323961a7dea53
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2025-04-26 17:56:55 +0800

    update

--- script.js
+++ script.js
@@ -14,9 +14,34 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(newBook);
 }
 
-addBookToLibrary('Hobbit','Juan',300,'read');
-console.log(myLibrary[0]);
+const main = document.querySelector('.main');
+const addButton = document.querySelector('#add');
+
+addBookToLibrary('Hobbit','Juan',300, true);
+
+addBookToLibrary('Book2','Author2',300, false);
+
+addBookToLibrary('Book3','Author3',300,true);
+
+addBookToLibrary('Book4','Author4',300,true);
 
 function displayBook(){
+    for(let i=0; i<myLibrary.length; i++){
+        const div = document.createElement('div');
+        main.appendChild(div);
+        div.innerHTML = 
+        `<div>Title:${myLibrary[i].title}</div>
+         <div>Author:${myLibrary[i].author}</div>
+         <div>Pages:${myLibrary[i].pages}</div>
+         <div>read:${myLibrary[i].read}</div>`
 
+    }
 }
+
+displayBook();
+
+addButton.addEventListener('click', (e) => {
+    // const div = document.createElement('div');
+    // main.appendChild(div);
+    // div.textContent = 'test';
+})
