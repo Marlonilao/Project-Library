dc2a5781de88bd28d3480a1dc615a03b4b157b5a
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2025-04-30 21:08:21 +0800

    update

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function Book(title, author, pages, read){
     this.title = title,
     this.author = author,
     this.pages = pages,
-    this.read = read,
-    this.id = self.crypto.randomUUID();
+    this.status = read,
+    this.id = self.crypto.randomUUID()
 }
 
 
@@ -16,32 +16,48 @@ function addBookToLibrary(title, author, pages, read) {
 
 const main = document.querySelector('.main');
 const addButton = document.querySelector('#add');
+const closeButton = document.querySelector('dialog button');
+const dialog = document.querySelector('dialog');
+const inputTitle = document.querySelector('input[name="title"]');
+const inputAuthor = document.querySelector('input[name="author"]');
+const inputPages = document.querySelector('input[name="pages"]');
+// const inputStatus = document.querySelector('input[name="status"]');
 
-addBookToLibrary('Hobbit','Juan',300, true);
-
-addBookToLibrary('Book2','Author2',300, false);
-
-addBookToLibrary('Book3','Author3',300,true);
-
-addBookToLibrary('Book4','Author4',300,true);
 
 function displayBook(){
-    for(let i=0; i<myLibrary.length; i++){
+    if (myLibrary.length > 1) {
         const div = document.createElement('div');
         main.appendChild(div);
         div.innerHTML = 
-        `<div>Title:${myLibrary[i].title}</div>
-         <div>Author:${myLibrary[i].author}</div>
-         <div>Pages:${myLibrary[i].pages}</div>
-         <div>read:${myLibrary[i].read}</div>`
-
+        `<div>Title:${myLibrary[myLibrary.length - 1].title}</div>
+        <div>Author:${myLibrary[myLibrary.length - 1].author}</div>
+        <div>Pages:${myLibrary[myLibrary.length - 1].pages}</div>
+        <div>Status:${myLibrary[myLibrary.length - 1].status}</div>`
+    } else {
+        const div = document.createElement('div');
+        main.appendChild(div);
+        div.innerHTML = 
+        `<div>Title:${myLibrary[0].title}</div>
+        <div>Author:${myLibrary[0].author}</div>
+        <div>Pages:${myLibrary[0].pages}</div>
+        <div>Status:${myLibrary[0].status}</div>`
     }
 }
 
-displayBook();
-
 addButton.addEventListener('click', (e) => {
-    // const div = document.createElement('div');
-    // main.appendChild(div);
-    // div.textContent = 'test';
+    dialog.showModal();
 })
+
+closeButton.addEventListener('click', e => {
+    e.preventDefault();
+    const author = inputAuthor.value;
+    const title = inputTitle.value;
+    const pages = inputPages.value;
+    const status = document.querySelector('input[name="status"]:checked').value;
+    addBookToLibrary(author, title, pages, status);
+    dialog.close();
+    displayBook();
+    document.querySelector('dialog form').reset();
+})
+
+
